b390f0fa5438583701d3808e554255248895c1ac
marioxaxa <melonston@gmail.com>
2023-01-22 16:13:45 -0300

    upload

--- src/routes.ts
+++ src/routes.ts
@@ -1,34 +1,63 @@
 import { FastifyInstance } from "fastify"
 import { prisma } from "./lib/prisma"
 // serve para validar dados passados para ele 
-import {z} from 'zod'
+import { z } from 'zod'
+import dayjs from "dayjs"
 
 
-export async function AppRoutes(app: FastifyInstance) {
+export async function appRoutes(app: FastifyInstance) {
     app.post('/habits', async (request) => {
 
         const createHabitBody = z.object({
-            title:z.string(),
+            title: z.string(),
             weekDays: z.array(z.number().min(0).max(6))
         })
-        const {title, weekDays} = createHabitBody.parse(request.body)
+        const { title, weekDays } = createHabitBody.parse(request.body)
 
-        const nomequalquer = prisma.habitWeekDays.create({
-            data:{week_day:2}
-        })
-
+        const today = dayjs().startOf('day').toDate()
 
         await prisma.habit.create({
-            data:{
+            data: {
                 title,
-                creation_date: new Date(),
-                weekDays:[],
-                dayHabit:[]
-
+                created_at: today,
+                weekDays: {
+                    create: weekDays.map((weekDay) => {
+                        return {
+                            week_day: weekDay,
+                        }
+                    }),
+                }
             }
         })
 
 
+    })
+
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query)
+
+        const weekDay = dayjs(date).get('day')
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+
+                creation_date: {
+                    lte: date,
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay
+                    }
+                }
+            }
+        })
+        return {
+            possibleHabits,
+        }
     })
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import Fastify from "fastify";
-import fastifyCors from "@fastify/cors";
-import { AppRoutes } from "./routes";
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from "./routes"
 
 const app = Fastify()
 
-app.register(fastifyCors)
-app.register(AppRoutes)
+app.register(cors)
+app.register(appRoutes)
 
 app.listen({
-    port: 3333
+  port: 3333,
 }).then(() => {
-    console.log('Server on')
+  console.log('HTTP Server running!')
 })
